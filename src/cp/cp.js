d677177b65a133c3c16f54081be5d706eb391427
Darya Shnipova <dshnipova@gmail.com>
2023-06-19 12:19:32 +0200

    feat: resolve child_process task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,13 @@
+import { spawn } from 'node:child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+	const file = 'src/cp/files/script.js';
+    const child = spawn(`node`, [file, ...args], { stdio: ['pipe', 'pipe', 'pipe', 'ipc'] });
+
+	process.stdin.pipe(child.stdin);
+
+	child.stdout.pipe(process.stdout);
+
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+await spawnChildProcess([1, 2, 3]);
